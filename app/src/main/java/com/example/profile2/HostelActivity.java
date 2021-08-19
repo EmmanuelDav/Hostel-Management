999d395da4c57765bc9e53c23a693f61d3c5a497
emmanueldav <kuradavis987@gmail.com>
2021-08-19 04:39:26 +0100

    seperating hostels based on gender

--- app/src/main/java/com/example/profile2/HostelActivity.java
+++ app/src/main/java/com/example/profile2/HostelActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.profile2.Adapter.RecyclerViewAdaptor;
 import com.example.profile2.model.Entry;
@@ -23,6 +24,8 @@ public class HostelActivity extends AppCompatActivity {
     FirebaseFirestore mFirebaseFirestore;
     List<Entry> mEntryList;
     ArrayList<String> ListOFHostels, flist;
+    String people;
+    TextView ifEmpty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +34,10 @@ public class HostelActivity extends AppCompatActivity {
         mFirebaseFirestore = FirebaseFirestore.getInstance();
         ListOFHostels = new ArrayList<>();
         mEntryList = new ArrayList<>();
+        ifEmpty = findViewById(R.id.empty);
         recyclerView = (RecyclerView) findViewById(R.id.home);
         recyclerView.setHasFixedSize(true);
+        people = getIntent().getExtras().getString("People");
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerViewAdaptor = new RecyclerViewAdaptor(HostelActivity.this, mEntryList);
         loadData();
@@ -52,28 +57,34 @@ public class HostelActivity extends AppCompatActivity {
                             String add, hf, nb, np, rp, rt, type, bed;
                             for (DocumentSnapshot i : li) {
                                 if (i.exists()) {
-                                    n = (String) i.get("Name");
-                                    ListOFHostels.add(n);
-                                    p = (String) i.get("Phone");
-                                    r = (String) i.getString("Rentp");
-                                    d = (String) i.getString("Distance");
-                                    add = (String) i.get("Address");
                                     hf = (String) i.get("HostelFor");
-                                    nb = (String) i.get("NumberB");
-                                    np = (String) i.get("NumberP");
-                                    rp = (String) i.get("Rentp");
-                                    rt = (String) i.get("Rentt");
-                                    type = (String) i.get("Type");
-                                    bed = (String) i.get("NoBedrooms");
-                                    String uu = (String) i.get("Url");
-                                    String lat = (String) i.get("Latitude");
-                                    String lon = (String) i.get("Longitute");
-                                    Entry entry = new Entry(n, p, r, d, R.drawable.ic_house, add, hf, nb, np, rp, rt, type, bed, uu, lat, lon);
-                                    mEntryList.add(entry);
-                                    recyclerView.setAdapter(mRecyclerViewAdaptor);
-                                    mRecyclerViewAdaptor.dataChange(HostelActivity.this, mEntryList);
+                                    assert hf != null;
+                                    if (hf.equals(people)) {
+                                        n = (String) i.get("Name");
+                                        ListOFHostels.add(n);
+                                        p = (String) i.get("Phone");
+                                        r = (String) i.getString("Rentp");
+                                        d = (String) i.getString("Distance");
+                                        add = (String) i.get("Address");
+                                        nb = (String) i.get("NumberB");
+                                        np = (String) i.get("NumberP");
+                                        rp = (String) i.get("Rentp");
+                                        rt = (String) i.get("Rentt");
+                                        type = (String) i.get("Type");
+                                        bed = (String) i.get("NoBedrooms");
+                                        String uu = (String) i.get("Url");
+                                        String lat = (String) i.get("Latitude");
+                                        String lon = (String) i.get("Longitute");
+                                        Entry entry = new Entry(n, p, r, d, R.drawable.ic_house, add, hf, nb, np, rp, rt, type, bed, uu, lat, lon);
+                                        mEntryList.add(entry);
+                                        recyclerView.setAdapter(mRecyclerViewAdaptor);
+                                        mRecyclerViewAdaptor.dataChange(HostelActivity.this, mEntryList);
+                                    }
                                 } else
                                     break;
+                            }
+                            if (mEntryList.isEmpty()){
+                                ifEmpty.setText("No Hostel Have been created for "+people);
                             }
                             mRecyclerViewAdaptor.copy();
                         }
---
+++ app/src/main/java/com/example/profile2/Hostel_Info.java
@@ -0,0 +1,30 @@
+package com.example.profile2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Hostel_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hostel_info);
+
+        findViewById(R.id.kings_hostel).setOnClickListener(vew -> {
+            Intent data = new Intent(this, HostelActivity.class);
+            data.putExtra("People","Boys");
+            startActivity(data);
+        });
+        findViewById(R.id.queens_hostel).setOnClickListener(view -> {
+
+            Intent data = new Intent(this, HostelActivity.class);
+            data.putExtra("People","Girls");
+            startActivity(data);
+        });
+        findViewById(R.id.available_rooms).setOnClickListener(view -> {
+            startActivity(new Intent(this, StudentActivity.class));
+        });
+    }
+}
